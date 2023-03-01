158f0dfb869e061be2db2234dc837094f1afea84
kalaiadi10 <114941188+kalaiadi10@users.noreply.github.com>
2023-03-01 03:17:04 -0800

    Add files via upload

---
+++ TestEncapsulation.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class TestEncapsulation 
+	{
+		public static void main(String[]args)
+	{
+			//creating object of class EncapsulationExample
+			EncapsulationExample a=new EncapsulationExample();
+			//object is used to access the method of the class
+			a.setId(1);
+			a.setName("kalai");
+			System.out.println("Id:"+a.getId()+"\n"+"Name:"+a.getName());
+		
+		
+	}
+	
+
+}
