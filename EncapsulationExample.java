8a4e01a8c910346ae06b4d6d07e0f08ead91f59c
kalaiadi10 <114941188+kalaiadi10@users.noreply.github.com>
2023-03-01 03:52:59 -0800

    Add files via upload

---
+++ EncapsulationExample.java
@@ -0,0 +1,26 @@
+package corejava;
+
+class EncapsulationExample
+{
+	//fields to calculate area
+
+
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	
+
+}
